80b600aaf6cb96d6ca052e0b5c5708d8705c92b0
diegocastilloupgrade <diego.castillo@bootcamp-upgrade.com>
2022-05-12 12:38:35 +0200

    Ejercicios 1.variables

--- S1/1.variables/ej7.js
+++ S1/1.variables/ej7.js
@@ -11,4 +11,5 @@ const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 
 globalBasePrice = 25000;
-console.log (car1.finalPrice = car1.basePrice + globalBasePrice, car2.finalPrice = car2.basePrice + globalBasePrice);
+
+console.log ("El precio final del", car1.name , "es", car1.finalPrice = car1.basePrice + globalBasePrice, "El precio del", car2.name, "es", car2.finalPrice = car2.basePrice + globalBasePrice);
