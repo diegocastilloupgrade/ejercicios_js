016c0c21d7c42e9a5f03c89a0dfe27bb928ddda4
diegocastilloupgrade <diego.castillo@bootcamp-upgrade.com>
2022-05-12 12:21:01 +0200

    Ejercicios Arrays

--- S1/3.arrays/ej2.js
+++ S1/3.arrays/ej2.js
@@ -1,5 +1,6 @@
 //Cambia el primer elemento de cars a "Ford"
 //
-//```js
-//const cars = ['Saab', 'Volvo', 'BMW'];
-//```
+
+const cars = ['Saab', 'Volvo', 'BMW'];
+cars.splice(0,1,"Ford");
+console.log(cars);
---
+++ S1/3.arrays/ej4.js
@@ -0,0 +1,6 @@
+// Añade 2 elementos al array: "Morty" y "Summer". Muestra en consola el último personaje del array
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+lastCharacterNumber = (rickAndMortyCharacters.length)-1;
+console.log(rickAndMortyCharacters[lastCharacterNumber]);
